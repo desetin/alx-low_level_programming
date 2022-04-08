75319456511921e19bb922e30b49b7450719305c
desetin <deseting.isangifi@gmail.com>
2022-04-07 21:57:28 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Functions begins
  *
- * Return: Functions ends
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -12,13 +12,12 @@ int main(void)
 	long int b;
 	long long int c;
 	char d;
-	float f;
+	float e;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(d));
-	printf("size of an int: %lu byte(s)\n", sizeof(a));
-	printf("size of a long int: %lu byte(s)\n", sizeof(b));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(c));
-	printf("size of a float: %lu byte(s)\n", sizeof(f));
-	
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
 	return (0);
 }
