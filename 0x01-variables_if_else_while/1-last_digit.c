cfea7950a1e5104e1e7d86175e82d5c218a65e10
desetin <deseting.isangifi@gmail.com>
2022-04-09 04:48:27 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d", n, n % 10);
+	printf("Last digit of %d is %d ", n, n % 10);
 	if (n % 10 > 5)
 	{
 		printf("and is greater than 5\n");
@@ -28,4 +28,5 @@ int main(void)
 		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
+
 }
