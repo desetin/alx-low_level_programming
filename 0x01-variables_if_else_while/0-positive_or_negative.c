555e8b1f4956735a777fc752d9146c8196c65aad
desetin <deseting.isangifi@gmail.com>
2022-04-09 04:15:14 -0700

    Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 /**
- * my_function - positive and negative intergers
+ * main - Entry point
  * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
