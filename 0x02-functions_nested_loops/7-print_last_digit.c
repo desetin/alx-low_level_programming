285030dfa495b3a64a93e41bef44fdbf76c9b609
desetin <deseting.isangifi@gmail.com>
2022-04-12 16:11:20 -0700

    print

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: is theint that will use for the argument of the function
+ * Return: integer value
+ */
+int print_last_value(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 10)
+		last = * -1;
+	_putchar(last + '0');
+	return (last);
+}
