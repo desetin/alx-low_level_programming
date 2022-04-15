73733fe8975a77a4a4b03657b95b833b88ffe502
desetin <deseting.isangifi@gmail.com>
2022-04-14 21:24:40 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int i;
 
-	/*printf("infinite loop incomming :(\n");*/
+	printf("infinite loop incomming :(\n");
 	i = 0
-		while (i < 10)
+		/*while (i < 10)
 		{
 			putchar(i);
-		}
+		}*/
 	printf("infinite loop avoided! \\o/\n");
 	return (0);
 }
