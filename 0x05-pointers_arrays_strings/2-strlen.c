61ec168cb89dd098799e92a09b98eb76209369b6
desetin <deseting.isangifi@gmail.com>
2022-04-21 06:30:32 -0700

    Return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: pointer to char
+ *
+ * Return: count
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	for(; *s != '\0'; s++)
+	{
+		count++;
+	}
+	return (count);
+}
