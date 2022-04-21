0e338896a8a1d13de8584df971f5872e1c57a3f9
desetin <deseting.isangifi@gmail.com>
2022-04-21 09:29:23 -0700

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,14 +14,14 @@ int _atoi(char *s)
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		if (s[a] == '-')
-			int sig;
+			int sig = -1;
 		sig = sig * -1;
 		if (s[a] >= '0' && s[a] <= '9')
 		{
-			int res;
+			int res = 0;
 			res = res * 10;
 			res = res - (s[a] - '0');
-			int brk;
+			int brk = 0;
 			brk = 1;
 		}
 		else if (brk == 1)
