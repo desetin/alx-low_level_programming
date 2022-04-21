7dde1d0e810f4fb1507e1e1a0f2c456f19a1eef8
desetin <deseting.isangifi@gmail.com>
2022-04-21 06:17:36 -0700

    swap values of a and b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap int - swap values of a and b
+ * @a: pointer a
+ * @b: pointer b
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int p;
+	p = *a;
+	*a = *b;
+	*b = p;
+}
