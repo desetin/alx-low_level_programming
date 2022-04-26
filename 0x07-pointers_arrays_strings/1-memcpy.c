b78677fe69ce78fc3c57e629aa16b7acbd81043a
desetin <deseting.isangifi@gmail.com>
2022-04-26 16:12:33 -0700

    copies memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0
+	i = 0;
 		while (i < n)
 		{
-			*(dest + i) = *(src + i);
-	i++;
+			dest[i] = src[i];
+			i++;
 		}
 	return (dest);
 }
