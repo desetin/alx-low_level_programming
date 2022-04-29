49fd4dbd8a0d3e95daba643fc61f619ffefd2403
desetin <deseting.isangifi@gmail.com>
2022-04-28 20:36:24 -0700

    natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * power_operation - function that reurns the natural square root of a number
+ * @n: input number
+ * @c: iterator
+ * Return: square rootn or -1
+ */
+
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input number
+ * Return: Square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (power_operation(n, 2));
+}
