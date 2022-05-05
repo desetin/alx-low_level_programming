1db3a5965494446ec380fe176553a6127fa842e3
desetin <deseting.isangifi@gmail.com>
2022-05-05 14:42:32 -0700

    return pointer to ne space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function that returns a pointer to a
+ * newly allocated space
+ * @str: input string
+ *
+ * Return: NULL if str == NULL
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, l = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[l] != '\0')
+		l++;
+	s = malloc((1 + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+	}
+	return (s);
+}
