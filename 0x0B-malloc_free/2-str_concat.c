a01d422471a485028ed494a6584b8bd069ab1642
desetin <deseting.isangifi@gmail.com>
2022-05-06 09:20:24 -0700

    concatenates 2 string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,15 +25,16 @@ char *str_concat(char *s1, char *s2)
 		b++;
 	s = malloc((a * sizeof(char)) + ((b + 1) * sizeof(char)));
 	if (s == NULL)
-		return (NULL)
+		return (NULL);
 			/* add first string to array s */
 			for (i = 0; s1[i] != '\0'; i++)
 				s[i] = s1[i];
 	/* addsecond string to area */
 	for (j = 0; s2[j] != '\0'; j++)
-[i] = s2[j];
-	i++;
-}
-s[i] = '\0';
-return (s);
+	{
+		s[i] = s2[j];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
 }
