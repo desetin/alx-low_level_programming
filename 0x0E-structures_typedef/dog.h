7d8a6150d286bad5022346b327fe8ce23e06e04d
desetin <deseting.isangifi@gmail.com>
2022-05-11 16:39:59 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog structure
+ * @name: input for dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* struct dog */
